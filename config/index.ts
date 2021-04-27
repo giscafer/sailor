d392b8bbf857c1667d2a8bb1b3e5e7b4491adfa8
giscafer <giscafer@outlook.com>
2021-04-27 17:43:20 +0800

    chore: 重构

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,7 @@
 export const appName = 'Sailor';
 
-export const BASE_URL = '//localhost:3000/sailor';
+// export const BASE_URL = '//localhost:3000/sailor';
+export const BASE_URL = '//api.bighome360.com/sailor';
 
 export const MENUS = [
     {
