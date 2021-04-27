4db2f38d9de228a14e4f0307c7ec494eeac6d9fa
giscafer <giscafer@outlook.com>
2021-04-27 18:09:03 +0800

    chore: 脚本

---
+++ prerun.js
@@ -0,0 +1,20 @@
+/**
+ * 根据环境变量修改服务地址
+ */
+
+const {readFileSync, writeFileSync} = require('fs');
+
+function prebuild(envType) {
+    const env = envType ?? (process.env.NODE_ENV || process.argv[2]);
+    console.log(env);
+    let envStr = "export const BASE_URL = '//localhost:3000/sailor';";
+    if (env === 'prod' || env === 'production') {
+        envStr = "export const BASE_URL = '//api.bighome360.com/sailor';";
+    }
+    const filePath = './config/index.ts';
+    const configContent = readFileSync(filePath).toString();
+    const newContent = configContent.replace(new RegExp("export const BASE_URL = '(.*)';", 'g'), envStr);
+    writeFileSync(filePath, newContent);
+}
+
+prebuild();
